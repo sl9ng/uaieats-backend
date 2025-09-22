ecad6a293ec4de77605fdd1cbf29123e110f896f
sl9ng <166817621+sl9ng@users.noreply.github.com>
2025-09-22 14:39:29 +0000

    url fix 7

--- backend/urls.py
+++ backend/urls.py
@@ -17,6 +17,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
     path('api/', include('api.urls')),
+    path('api/', include('app.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('cards/', CardListCreateView.as_view(), name='card_list_create'),
